b3008f1e872c99a4e14e526bb014c5ea52ac0e21
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2022-08-27 18:02:25 +0530

    sorry

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Search from '../components/search/search'
 export default function App() {
+    /*
+        Sorry ,Time was not enough 
+    */
     return (
         <>
             <header className='_header'>
