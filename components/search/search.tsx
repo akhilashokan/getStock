835feb82a6a3993d7bfc8b42cdbf0403ae0fe29c
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2022-08-27 18:00:29 +0530

    search test data

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -1,11 +1,48 @@
 import style from './search.module.scss'
 export default function Search() {
+    /*
+    since the time has reached setting 
+    a variable to show what i would have 
+    done when i got the data. :|
+    */
+    var stocks = [
+        "Bhansali Engg",
+        "Coal Indi",
+        "IOL Chemical",
+        "Dolat Investmen",
+        "NDT",
+        "Balmer Law Inv",
+        "Ebixcash Worl",
+        "Mangalam Organi",
+        "INEOS Styrolut",
+        "Expleo Solution",
+        "Sh Jagdamba Po",
+        "Godawari Powe",
+        "Cigniti Tech",
+        "Kirl Ferrou",
+        "Rite",
+        "GujStPetrone",
+        "Ester Industrie",
+        "Anjani Portlan",
+        "Venky's (India",
+        "Heritage Food-",
+        "SI",
+        "GTPL Hathwa",
+        "Welspun Cor",
+        "I G Petrochem",
+        "Geojit Fin Ser",
+        "Sasken Technol",
+        "Engineers Indi",
+        "Petronet LN",
+        "Saksof",
+        "Polyplex Corp"
+    ];
 
     return (
         <div className={style.container}>
             <input className={style.search} type="text" list="stocks" name="" id="" />
             <datalist id="stocks">
-                <option value="stockName">stock</option>
+                {stocks.map((value, key) => <option key={key} value={value}>{value}</option>)}
             </datalist>
         </div>
     )
