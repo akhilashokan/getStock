830963f41c6ccb42003ee7f2f5ac94858f727a62
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2022-09-07 14:28:43 +0530

    card display done + search clear added

--- components/display/display.tsx
+++ components/display/display.tsx
@@ -1,23 +1,32 @@
+import { useEffect, useState } from "react"
 import style from "./display.module.scss"
 export default function Display({ item }) {
+    const [loading, setLoading] = useState(true)
     const stats = Object.entries(item).filter((item: [string, string]) => {
         let found = false
         if (item[0] === 'S.No.' || item[0] === 'Name') found = true
         return found ? null : item
     })
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000);
+    }, [])
     return (
         <div className={`card ${style.card}`}>
-            <h2>{item.Name}</h2>
-            <div className="wrapper">
-                <div className={style.stats}>
-                    {stats.map((value: [string, string], key) => {
-                        return <div className={style.stat} key={key}>
-                            <p className={style.statName}>{value[0]}</p>
-                            <p className={style.value}>&#2352; {value[1]}</p>
-                        </div>
-                    })}
+            {loading ? <p>loading</p> : <>
+                <h2>{item.Name}</h2>
+                <div className="wrapper">
+                    <div className={style.stats}>
+                        {stats.map((value: [string, string], key) => {
+                            return <div className={style.stat} key={key}>
+                                <p className={style.statName}>{value[0]}</p>
+                                <p className={style.value}>&#2352; {value[1]}</p>
+                            </div>
+                        })}
+                    </div>
                 </div>
-            </div>
+            </>}
         </div>
     )
 }
--- components/search/search.tsx
+++ components/search/search.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Display from '../display/display'
 import style from './search.module.scss'
 export default function Search() {
     const [stocks, setStocks] = useState([{
         "Name": "",
     },])
+    const ref = useRef()
     const [selectStock, setSelectedStock] = useState(null)
+    const [clear, setClear] = useState(false)
     useEffect(() => {
         const options = { method: 'GET', headers: { Accept: 'application/json' } };
         fetch('http://localhost:8888/.netlify/functions/getdata', options)
@@ -19,16 +21,25 @@ export default function Search() {
         stocks.find((item) => {
             if (item.Name === value) {
                 setSelectedStock(item)
+                setClear(true)
             }
         })
+    }
+    function handleClear() {
+        setSelectedStock(null)
+        setClear(false)
+        if (ref) {
+            ref.current.value = ''
+        }
     }
     return (
         <div className={style.container}>
             <div className="input">
-                <input className={style.search} onChange={handleChange} type="text" list="stocks" name="" id="" />
+                <input className={style.search} ref={ref} onChange={handleChange} type="text" list="stocks" />
                 <datalist id="stocks">
                     {stocks.map((value, key) => <option key={key} value={value.Name}>{value.Name}</option>)}
                 </datalist>
+                {clear && <p onClick={handleClear} className={style.clear}>clear</p>}
             </div>
             {selectStock && <Display item={selectStock} />}
         </div>
